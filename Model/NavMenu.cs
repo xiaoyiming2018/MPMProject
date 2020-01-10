5397f935f513aee1aa55c633c30994b8dc1e3b4a
yi.luo <yi.luo@advantech.com.cn>
2020-01-10 13:52:29 +0800

    11

---
+++ Model/NavMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class NavMenu
+    {
+        public string Name { get; set; }
+        public string TemplateUrl { get; set; }
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 子菜单
+        /// </summary>
+        public IList<NavMenu> SubNavMenus { get; set; }
+    }
+
+    /// <summary>
+    /// 左侧导航菜单视图模型
+    /// </summary>
+    public class NavBarMenus
+    {
+        public IList<NavMenu> NavMenus { get; set; }
+    }
+}
